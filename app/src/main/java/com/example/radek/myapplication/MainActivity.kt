fe85d6769ee3b70ad3eda6394a83d776712e3f51
Radzio <radekborowicz@outlook.com>
2018-01-09 00:48:22 +0100

    Hello World !

--- app/src/main/java/com/example/radek/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/radek/myapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.radek.myapplication
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val welcomeText = welcomeText
+        welcomeText.text = getString(R.string.welcomeText)
     }
 }
